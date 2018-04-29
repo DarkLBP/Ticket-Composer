24390824cd56ec833e7d599d0e0d9f00627849ff
DarkLBP <darklbp@outlook.com>
2018-04-29 20:54:46 +0200

    Upgraded EasyMVC to 0.7.2

--- core/View.php
+++ core/View.php
@@ -12,22 +12,29 @@ class View
     public function __construct(string $view, string $controller = '')
     {
         //If controller is specified check if that controller has the view if not try to load it in the root
-        $headerView = __DIR__ . '/../views/' . $controller . '/HeaderView.php';
-        $contentView = __DIR__ . '/../views/' . $controller . '/' . ucfirst($view) . 'View.php';
-        $footerView = __DIR__ . '/../views/' . $controller . '/FooterView.php';
-        if (file_exists($headerView)) {
-            $this->headerFile = $headerView;
-        }
-        if (file_exists($contentView)) {
-            $this->contentFile = $contentView;
-        } else {
-            $fallBack = __DIR__ . '/../views/' . ucfirst($view) . 'View.php';
-            if (file_exists($fallBack)) {
-                $this->contentFile = $fallBack;
+        if (!empty($controller)) {
+            $headerView = __DIR__ . '/../views/' . $controller . '/HeaderView.php';
+            $contentView = __DIR__ . '/../views/' . $controller . '/' . ucfirst($view) . 'View.php';
+            $footerView = __DIR__ . '/../views/' . $controller . '/FooterView.php';
+            if (file_exists($headerView)) {
+                $this->headerFile = $headerView;
             }
-        }
-        if (file_exists($footerView)) {
-            $this->footerFile = $footerView;
+            if (file_exists($contentView)) {
+                $this->contentFile = $contentView;
+            } else {
+                $fallBack = __DIR__ . '/../views/' . ucfirst($view) . 'View.php';
+                if (file_exists($fallBack)) {
+                    $this->contentFile = $fallBack;
+                }
+            }
+            if (file_exists($footerView)) {
+                $this->footerFile = $footerView;
+            }
+        } else {
+            $contentView = __DIR__ . '/../views/' . ucfirst($view) . 'View.php';
+            if (file_exists($contentView)) {
+                $this->contentFile = $contentView;
+            }
         }
     }
 
@@ -41,13 +48,33 @@ class View
     }
 
     /**
-     * Renders the full view
+     * Renders the view
+     * @param bool $header Whether the header will be shown
+     * @param bool $footer Whether the footer will be shown
      */
-    public function show(): void
+    public function show(bool $header = true, bool $footer = true): void
     {
         extract($this->params);
-        include_once $this->headerFile;
-        include_once $this->contentFile;
-        include_once $this->footerFile;
+        if (file_exists($this->headerFile) && $header) {
+            include_once $this->headerFile;
+        }
+        if (file_exists($this->contentFile)) {
+            include_once $this->contentFile;
+        }
+        if (file_exists($this->footerFile) && $footer) {
+            include_once $this->footerFile;
+        }
+    }
+
+    /**
+     * Includes an additional view without header and footer
+     * @param string $view The view to be loaded
+     * @param string $controller The controller that owns the view
+     */
+    private function renderExtra(string $view, string $controller = ''): void
+    {
+        $view = new View($view, $controller);
+        $view->setParams($this->params);
+        $view->show(false, false);
     }
 }
