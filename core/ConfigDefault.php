6d906cce7f0c9d8ecff5bc55f6888afbe2ba9554
DarkLBP <darklbp@outlook.com>
2018-05-07 19:59:27 +0200

    Upgraded EasyMVC to 0.8.6

---
+++ core/ConfigDefault.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define("SITE_TITLE", "Ticket Composer");
+define("DEFAULT_CONTROLLER", "main");
+define("DEFAULT_ACTION", "index");
+
+spl_autoload_register(function ($class) {
+    $segments = explode("\\", $class);
+    $path = '';
+    for ($i = 0; $i < count($segments) - 1; $i++) {
+        $path .= strtolower($segments[$i]) . '/';
+    }
+    $finalPath = __DIR__ . '/../' . $path . '/' . $segments[$i] . '.php';
+    if (file_exists($finalPath)) {
+        include_once $finalPath;
+    }
+});
+
+//define("DATABASE_HOST", "localhost");
+//define("DATABASE_USER", "");
+//define("DATABASE_PASSWORD", "");
+//define("DATABASE_DB", "");
+
--- site/index.php
+++ site/index.php
@@ -1,10 +1,22 @@
 <?php
 session_start();
-require_once __DIR__ . '/../core/Config.php';
-$request = new \Core\Request();
-$session = new \Controllers\SessionController($request);
-$session->initialize();
-$request->setViewParam('loggedUser', $request->getSessionParam('loggedUser'));
-$request->setViewParam('controller', $request->getController());
-$request->setViewParam('action', $request->getAction());
+
+if (file_exists( __DIR__ . '/../core/Config.php')) {
+    require_once __DIR__ . '/../core/Config.php';
+    $request = new \Core\Request();
+    $session = new \Controllers\SessionController($request);
+    $session->initialize();
+    $request->setViewParam('loggedUser', $request->getSessionParam('loggedUser'));
+    $request->setViewParam('controller', $request->getController());
+    $request->setViewParam('action', $request->getAction());
+} else {
+    require_once __DIR__ . '/../core/ConfigDefault.php';
+    $request = new \Core\Request();
+    if ($request->getController() != "install") {
+        $request->redirect(\Core\Utils::getURL('install'));
+    }
+    $request->setViewParam('loggedUser', "");
+    $request->setViewParam('controller', $request->getController());
+    $request->setViewParam('action', $request->getAction());
+}
 $request->dispatch();
