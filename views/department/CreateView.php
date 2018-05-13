eb5e30ac56b1809b2dd57b9ff80f9675176515d4
DarkLBP <darklbp@outlook.com>
2018-05-13 17:59:27 +0200

    Correct submit button position

--- views/department/CreateView.php
+++ views/department/CreateView.php
@@ -5,7 +5,7 @@ use Core\Utils;
     <h2>New Department</h2>
     <?= !empty($error) ? "<p class='error-message'>$error</p>" : '' ?>
     <form action="<?= Utils::getURL('department', 'create') ?>" method="post">
-        <label for="name">Name:</label><br>
+        <label for="name">Name</label><br>
         <input type="text" name="name" id="name" required><br>
         <input type="submit" value="Create Department">
     </form>
